1b2ee4e5f4246578fae719dc49672fe0a5227a64
Gitroars <501sterror@gmail.com>
2021-12-20 12:23:55 +0700

    Update PDM.py

--- PDM.py
+++ PDM.py
@@ -1,13 +1,13 @@
 def calculate_salary():
-    x = 0
+    x = -1
     y = 0
     #inputing the numbers
-    while x<=0:
+    while x<0 or x>20:
         x = int(input("Input the number of overtime hours: "))
         if x >= 0 and x<=20:
             break
         print("Should be a positive integer below 21")
-    while y<=0:    
+    while y<=0 or y>3:
         y = int(input("Input the level of allowance: "))
         if y > 0 and y<=3:
             break
@@ -37,12 +37,12 @@ def calculate_salary():
             groups.append(s[-3:])
             s = s[:-3]
         return s + '.'.join(reversed(groups))
-    print("------------------------------------------")
+    print("________________________")
     print("Overtime Pay: {}".format(group(overtimePay)))
     print("Basic Salary: {}".format(group(3800000)))
     print("Allowance: {}".format(group(allowance)))
+    print("________________________")
     print("Total Salary: {}".format(group(result)))
-    print("------------------------------------------")
 
 if __name__ == '__main__':
     calculate_salary()
